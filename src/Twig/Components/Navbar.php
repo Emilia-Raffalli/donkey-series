45062f38357a05a53382573ae7957ccc95c97c8b
Emilia RAFFALLI <e.raffalli@hotmail.fr>
2023-12-29 13:16:24 +0100

    Creation of Navbar with twig component

---
+++ src/Twig/Components/Navbar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Repository\CategoryRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent]
+final class Navbar
+{
+    use DefaultActionTrait;
+
+    private CategoryRepository $categoryRepository;
+    public array $categories = [];
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    public function mount(): array
+    {
+        $this->categories = $this->categoryRepository->findAll();
+
+        return ['categories' => $this->categories];
+    }
+}
